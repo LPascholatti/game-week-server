12f03a8f5a0d95b70b6dcfb15954be4471fb55d4
LPascholatti <lucaspascholatti@gmail.com>
2019-10-07 14:34:50 +0200

    Added midlewares for the user and auth routers, installed jwt

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ const bodyParser = require('body-parser')
 const jsonParser = bodyParser.json()
 const cors = require('cors')
 const corsMiddleware = cors()
+const userRouter = require('./user/router')
+const authRouter = require('./auth/router')
 
 app.use(corsMiddleware)
 app.use(jsonParser)
+app.use(authRouter)
+app.use(userRouter)
 
 function onListen() {
   console.log(`Server running on port ${port}`)
