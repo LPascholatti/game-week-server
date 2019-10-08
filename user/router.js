bb5a16be7f42460b2c15acca19b61760ff25e2e5
debesteste <gersonlynch@gmail.com>
2019-10-08 14:54:32 +0200

    Added /user request with findall()

--- user/router.js
+++ user/router.js
@@ -17,6 +17,12 @@ router.post('/user', (req, res, next) => {
     .catch(next)
 })
 
+router.get('/user', (req, res, next) => {
+  User
+  .findAll()
+  .then(user => res.json(user))
+  .catch(next)
+})
 
 router.get('/', (request, response )=>{
   console.log("got an get request on /")
