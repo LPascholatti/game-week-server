d54b4ca31cf37b71f4e24233a9d1b59dcbc8ecc5
LPascholatti <lucaspascholatti@gmail.com>
2019-10-08 13:20:56 +0200

    Introducing the stream and json-sse

--- index.js
+++ index.js
@@ -7,11 +7,25 @@ const cors = require('cors')
 const corsMiddleware = cors()
 const userRouter = require('./user/router')
 const authRouter = require('./auth/router')
+const Sse = require('json-sse')
+
+const stream = new Sse()
+console.log('stream', stream)
 
 app.use(corsMiddleware)
 app.use(jsonParser)
 app.use(authRouter)
 app.use(userRouter)
+
+app.get('/stream', async (req, res) => {
+  console.log('got a request for a stream')
+  const room = await User.findAll()
+  const data = JSON.stringify(room)
+  console.log("messages in this room are:", data)
+
+  stream.updateInit(data)
+  stream.init(req, res)
+})
 
 function onListen() {
   console.log(`Server running on port ${port}`)
